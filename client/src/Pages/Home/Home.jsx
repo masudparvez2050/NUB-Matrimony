7523fdca28315c7a926f3006bc76ad3f1825cef5
helalkhandev <helal.khan2013@gmail.com>
2023-10-18 01:09:22 +0600

    Change Heaer etc

--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -8,6 +8,18 @@ function Home() {
   return (
     <div>
       {" "}
+      <section className="mb-40">
+        <div className="bg-neutral-50 py-24 px-6 text-center dark:bg-neutral-900">
+          <h1 className="mt-2 mb-16 text-5xl font-bold tracking-tight md:text-6xl xl:text-7xl">
+            The best offer on the market <br /><span className="text-primary">for your business</span>
+          </h1>
+          <a className="mb-2 inline-block rounded bg-primary px-12 pt-4 pb-3.5 text-sm font-medium uppercase leading-normal text-Red shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] md:mr-2 md:mb-0"
+            data-te-ripple-init data-te-ripple-color="light" href="#!" role="button">Get started</a>
+          <a className="inline-block rounded px-12 pt-4 pb-3.5 text-sm font-medium uppercase leading-normal text-primary transition duration-150 ease-in-out hover:bg-neutral-500 hover:bg-opacity-10 hover:text-primary-600 focus:text-primary-600 focus:outline-none focus:ring-0 active:text-primary-700 dark:hover:bg-neutral-800 dark:hover:bg-opacity-60"
+            data-te-ripple-init data-te-ripple-color="light" href="#!" role="button">Learn more</a>
+        </div>
+        </section>
+
       <Header />
       <h1 className="text-3xl font-bold underline">This is Home page.</h1>
       <h1 className="text-3xl font-bold underline">এটি একটি হোম পেজ</h1>
