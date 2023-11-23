ab54e1d0a5eeb3a0681cd01c67d1fba9a693935d
helalkhandev <helal.khan2013@gmail.com>
2023-11-23 20:04:43 +0600

    team

--- client/src/Components/Layout/Header.jsx
+++ client/src/Components/Layout/Header.jsx
@@ -35,7 +35,7 @@ const Header = () => {
         <div className="flex items-center">
           <Link to="/">
             <img
-              src="/src/assets/images/Logo.png" // Add your logo source here
+              src= {changeLanguage ? "/src/assets/images/Metrimonititle_Bangla-v2.png" : "/src/assets/images/Logo.png"}  // Add your logo source here
               alt="Logo"
               className=" h-[5rem] w-[12rem] ml-3 mt-2" // Customize the logo size
             />
--- client/src/Pages/Team/Team.jsx
+++ client/src/Pages/Team/Team.jsx
@@ -5,7 +5,14 @@ function Team() {
   return (
     <>
       <Header />
-      <div>This is Team page</div>
+      <div>
+        
+        <div className="p-20 bg-[#a1b9d9]">
+        <h1 className="text-center font-bold text-6xl text-white hover:text-7xl transition-all duration-700">Our Team Member</h1>
+        </div>
+        
+        
+        </div>
       <Footer />
     </>
   );
