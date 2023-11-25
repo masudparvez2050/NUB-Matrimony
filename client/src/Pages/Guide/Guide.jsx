ffb8931ca7323a4eae21d378a0de5e9d884134d4
MASUDUR RAHMAN <masudparvez2050@gmail.com>
2023-11-25 18:58:53 +0600

    updated

--- client/src/Pages/Guide/Guide.jsx
+++ client/src/Pages/Guide/Guide.jsx
@@ -7,32 +7,32 @@ const itemsEn = [
   {
     title: "How to create an account in NUB Metrimony?",
     content:
-      "1. First, go to NUB Metrimony and then click on the user icon at the upper right corner of the home page. 2. Click Create Account. 3. Registration Form will be displayed on the screen. Enter your name and select your gender. 4. Enter your email address and click the Verify button. If your email address is correct, there will be a verification code in the email box. After completing the email verification, enter the verification code in the specified field and click on the Confirm button. 5. Enter your mobile phone number and click the Verify button. If your number is correct, there will be a verification code. Enter the verification code in the specified field and click on the Confirm button. 6. Choose a password and confirm your password. 7. Check the Checkbox if you agree to the Terms and Conditions and Privacy Policyof NUB Metrimony. 8. Complete the account by clicking the Create Account button.",
+      "1. First, go to NUB Metrimony and then click on the user icon at the upper right corner of the home page. <br> 2. Click Create Account. <br> 3. Registration Form will be displayed on the screen. Enter your name and select your gender. <br>  4. Enter your email address and click the Verify button. If your email address is correct, there will be a verification code in the email box. After completing the email verification, enter the verification code in the specified field and click on the Confirm button. <br> 5. Enter your mobile phone number and click the Verify button. If your number is correct, there will be a verification code. Enter the verification code in the specified field and click on the Confirm button. <br> 6. Choose a password and confirm your password. <br> 7. Check the Checkbox if you agree to the Terms and Conditions and Privacy Policyof NUB Metrimony. <br> 8. Complete the account by clicking the Create Account button.",
   },
   {
     title: "How to submit biodata in NUB Metrimony ?",
     content:
-      "1. First, log in at NUB Metrimony. Create an account if you don't have an account on NUB Metrimony. 2. Then click the Create Biodata button from the home page or dashboard. 3. Before creating the biodata, read the terms and conditions of NUB Metrimony and if you agree, check all the Checkbox and click the Create Biodata button. 4. Biodata form will be displayed. After filling in all the information, you will enter the Biodata Review Page and submit the biodata by clicking on the Submit button after checking all the information carefully. 5. Within a few days, the NUB Metrimony Customer Care will call your parents and you for verification. 6. After reviewing your biodata by NUB Metrimony Customer Care, you will be notified by email about the 'Approved' or 'Not-approved' status in sha Allah. Note : Write all the information clearly in the biodata",
+      "1. First, log in at NUB Metrimony. Create an account if you don't have an account on NUB Metrimony. <br> 2. Then click the Create Biodata button from the home page or dashboard. <br> 3. Before creating the biodata, read the terms and conditions of NUB Metrimony and if you agree, check all the Checkbox and click the Create Biodata button. <br> 4. Biodata form will be displayed. After filling in all the information, you will enter the Biodata Review Page and submit the biodata by clicking on the Submit button after checking all the information carefully. <br> 5. Within a few days, the NUB Metrimony Customer Care will call your parents and you for verification. <br> 6. After reviewing your biodata by NUB Metrimony Customer Care, you will be notified by email about the 'Approved' or 'Not-approved' status in sha Allah. Note : Write all the information clearly in the biodata",
   },
   {
     title: "How to edit biodata in NUB Metrimony?",
     content:
-      "1. First, log in to your account. 2. Then, click the user icon at the top right corner of the page. 3. Click the Edit Biodata button. 4. The biodata form will be displayed. Review your biodata after editing the information you need and then click Submit button. 5. The NUB Metrimony Customer Care will review the information you have edited. 6. After reviewing your biodata by NUB Metrimony Customer Care, you will be notified by email about the 'Approved' or 'Not-approved' status In Sha Allah.",
+      "1. First, log in to your account. <br> 2. Then, click the user icon at the top right corner of the page. <br> 3. Click the Edit Biodata button. <br> 4. The biodata form will be displayed. Review your biodata after editing the information you need and then click Submit button. <br> 5. The NUB Metrimony Customer Care will review the information you have edited. <br> 6. After reviewing your biodata by NUB Metrimony Customer Care, you will be notified by email about the 'Approved' or 'Not-approved' status In Sha Allah.",
   },
   {
     title: "How to temporarily hide biodata in NUB Metrimony?",
     content:
-      "Keep your biodata hidden during the conversation for marriage issues with the marital alliance. Moreover, you can hide biodata for any reason with one click for any duration and your biodata will not be shown in search results. 1. First, log in to the account. 2. After logging in, go to the Dashboard or click the user icon at the top right corner. 3. Click the Slide Button under Biodata Status. 4. A Confirmation Popup will be shown. 5. Hide the biodata by clicking on the OK button. In the same way, you can make the biodata live again at any time. However, if any information is edited, Customer Care will review your biodata again.",
+      "Keep your biodata hidden during the conversation for marriage issues with the marital alliance. Moreover, you can hide biodata for any reason with one click for any duration and your biodata will not be shown in search results. <br> 1. First, log in to the account. <br> 2. After logging in, go to the Dashboard or click the user icon at the top right corner. <br> 3. Click the Slide Button under Biodata Status. <br> 4. A Confirmation Popup will be shown. <br> 5. Hide the biodata by clicking on the OK button. In the same way, you can make the biodata live again at any time. However, if any information is edited, Customer Care will review your biodata again.",
   },
   {
     title: "How to delete biodata in NUB Metrimony ?",
     content:
-      "You can delete your biodata yourself at any time. 1. First, log in to your account. 2. Go to the dashboard after logging in and click the user icon at the upper right corner. 3. Click Settings. 4. Delete your biodata by clicking on the Delete Biodata button. Note: Once your biodata is deleted, it cannot be undone. You can hide it if necessary for a temporary period.",
+      "You can delete your biodata yourself at any time. <br>  1. First, log in to your account. <br>  2. Go to the dashboard after logging in and click the user icon at the upper right corner. <br>  3. Click Settings. <br>  4. Delete your biodata by clicking on the Delete Biodata button.  <br> Note: Once your biodata is deleted, it cannot be undone. You can hide it if necessary for a temporary period.",
   },
   {
     title: "How will I complain about a biodata?",
     content:
-      "After viewing the contact information of your biodata through the Connection, if you want to make a complaint about that biodata to NUB Metrimony. 1. Click on the Report button at the bottom of the biodata page. 2. Next, submit the complaint-related information in the form on the next page. Customer Care will verify it and take proper actions and inform you shortly in sha Allah.",
+      "After viewing the contact information of your biodata through the Connection, if you want to make a complaint about that biodata to NUB Metrimony. <br> 1. Click on the Report button at the bottom of the biodata page. <br> 2. Next, submit the complaint-related information in the form on the next page. <br> Customer Care will verify it and take proper actions and inform you shortly in sha Allah.",
   },
   {
     title: "How to purchase connection?",
@@ -102,6 +102,14 @@ const itemsBn = [
   },
 ];
 
+const ItemComponent = ({ content }) => {
+  return (
+    <div>
+      <div dangerouslySetInnerHTML={{ __html: content }} />
+    </div>
+  );
+};
+
 function Guide() {
   const { changeLanguage } = useLanguage();
   const [activeIndex, setActiveIndex] = useState(null);
@@ -130,16 +138,20 @@ function Guide() {
                 <div key={index} className="mb-2">
                   <button
                     className="w-full bg-white drop-shadow p-5 flex justify-between items-center hover:bg-gray-100 rounded-md text-left"
-                    onClick={() => handleClick(index)}>
+                    onClick={() => handleClick(index)}
+                  >
                     <span>{item.title}</span>
                     <span className="text-lg transition-all duration-500 ease-in-out">
                       {activeIndex === index ? "-" : "+"}
                     </span>
                   </button>
                   {activeIndex === index && (
-                    <div className="p-4 bg-white transition-all duration-500 ease-in-out transition-max-h">
+                    <>
+                      <ItemComponent key={index} {...item} />
+                      {/* <div className="p-4 bg-white transition-all duration-500 ease-in-out transition-max-h">
                       {item.content}
-                    </div>
+                    </div> */}
+                    </>
                   )}
                 </div>
               ))}
@@ -150,16 +162,20 @@ function Guide() {
                 <div key={index} className="mb-2">
                   <button
                     className="w-full bg-white drop-shadow p-5 flex justify-between items-center hover:bg-gray-100 rounded-md text-left"
-                    onClick={() => handleClick(index)}>
+                    onClick={() => handleClick(index)}
+                  >
                     <span>{item.title}</span>
                     <span className="text-lg transition-all duration-500 ease-in-out">
                       {activeIndex === index ? "-" : "+"}
                     </span>
                   </button>
                   {activeIndex === index && (
-                    <div className="p-4 bg-white transition-all duration-500 ease-in-out transition-max-h">
+                    <>
+                      <ItemComponent key={index} {...item} />
+                      {/* <div className="p-4 bg-white transition-all duration-500 ease-in-out transition-max-h">
                       {item.content}
-                    </div>
+                    </div> */}
+                    </>
                   )}
                 </div>
               ))}
