32d029dd019603afb9de8fc344fdd8cd31f96c2a
helalkhandev <helal.khan2013@gmail.com>
2023-11-28 16:35:03 +0600

    change

--- client/src/Pages/Contact/Contact.jsx
+++ client/src/Pages/Contact/Contact.jsx
@@ -16,8 +16,7 @@ function Contact() {
             <div className="mb-5">
               <label
                 htmlFor="name"
-                className="mb-3 block text-base font-medium text-[#07074D]"
-              >
+                className="mb-3 block text-base font-medium text-[#07074D]">
                 Full Name
               </label>
               <input
@@ -31,8 +30,7 @@ function Contact() {
             <div className="mb-5">
               <label
                 for="email"
-                className="mb-3 block text-base font-medium text-[#07074D]"
-              >
+                className="mb-3 block text-base font-medium text-[#07074D]">
                 Email Address
               </label>
               <input
@@ -46,8 +44,7 @@ function Contact() {
             <div className="mb-5">
               <label
                 for="subject"
-                className="mb-3 block text-base font-medium text-[#07074D]"
-              >
+                className="mb-3 block text-base font-medium text-[#07074D]">
                 Subject
               </label>
               <input
@@ -61,8 +58,7 @@ function Contact() {
             <div className="mb-5">
               <label
                 for="message"
-                className="mb-3 block text-base font-medium text-[#07074D]"
-              >
+                className="mb-3 block text-base font-medium text-[#07074D]">
                 Message
               </label>
               <textarea
@@ -70,8 +66,7 @@ function Contact() {
                 name="message"
                 id="message"
                 placeholder="Type your message"
-                className="w-full resize-none rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
-              ></textarea>
+                className="w-full resize-none rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"></textarea>
             </div>
             <div>
               <button className="bg-rose-700 hover:bg-rose-600 rounded-md py-3 px-8 text-base font-semibold text-white outline-none">
